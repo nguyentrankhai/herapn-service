8921b1d80619f459fd80fcc99094b7003bd736d5
nguyentk <nguyen.trankhai0111@gmail.com>
2024-01-10 23:00:10 +0700

    change ecosystem file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -23,7 +23,7 @@ module.exports = {
       user: 'developer', // Người dùng để kết nối SSH
       host: '192.168.123.123', // Địa chỉ IP hoặc tên miền của máy chủ
       ref: 'origin/main', // Nhánh bạn muốn triển khai
-      repo: 'git@github.com:nguyenduongbichduyen/HeraPN.git', // URL repo của dự án
+      repo: 'git@github.com:nguyentrankhai/herapn-service.git', // URL repo của dự án
       path: '/home/developer/herapn/server', // Thư mục bạn muốn triển khai ứng dụng vào
       'post-deploy': 'cd ./SourceCode/server && npm install && pm2 reload ecosystem.config.js --env production',
     },
